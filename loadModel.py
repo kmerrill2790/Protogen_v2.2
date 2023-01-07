d30de065e4cac81556c79a11c09df7b724edca06
zK <kmerrill2790@gmail.com>
2023-01-06 20:20:52 -0800

    Update loadModel.py

--- loadModel.py
+++ loadModel.py
@@ -17,6 +17,7 @@ MODEL_IDS = [
     "stabilityai/stable-diffusion-2",
     "stabilityai/stable-diffusion-2-base",
     "stabilityai/stable-diffusion-2-inpainting",
+    "darkstorm2150/Protogen_v2.2_Official_Release",
 ]
 
 
